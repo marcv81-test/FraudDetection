190a17e975eae02e8a784b7216ef445a0db55efd
Marc <marc.venturini@gmail.com>
2018-04-02 22:56:12 +0800

    Extract click_id from test dataset

--- convert.py
+++ convert.py
@@ -27,5 +27,16 @@ def csv_to_hdf5(csv_file, hdf5_file, columns):
     store.close()
     gc.collect()
 
+def click_id_to_hdf5(csv_file, hdf5_file):
+    """Loads the click_id field of a CSV dataset. Saves as HDF5."""
+    print('Loading', csv_file)
+    dataset = pandas.read_csv(csv_file, usecols=['click_id'])
+    print('Saving', hdf5_file)
+    store = pandas.HDFStore(hdf5_file)
+    store.put('dataset', dataset)
+    store.close()
+    gc.collect()
+
 csv_to_hdf5('train.csv', 'train.h5', train_columns)
 csv_to_hdf5('test.csv', 'test.h5', test_columns)
+click_id_to_hdf5('test.csv', 'test_id.h5')
